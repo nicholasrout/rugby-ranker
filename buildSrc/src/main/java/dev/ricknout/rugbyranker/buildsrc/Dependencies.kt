72b9fad602c481dc61cd3a754e9e0900b41098a9
Nick Rout <nickrout@google.com>
2020-09-18 16:49:00 +0200

    Prepare news module for Compose migration

--- buildSrc/src/main/java/dev/ricknout/rugbyranker/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/dev/ricknout/rugbyranker/buildsrc/Dependencies.kt
@@ -8,6 +8,7 @@ object Versions {
     const val ktlint = "0.39.0"
     const val coil = "1.0.0-rc2"
     const val insetter = "0.3.1"
+    const val accompanist = "0.2.2"
     object Kotlin {
         const val kotlin = "1.4.10"
         const val coroutines = "1.3.9"
@@ -166,5 +167,8 @@ object Libs {
     }
     object Insetter {
         const val ktx = "dev.chrisbanes:insetter-ktx:${Versions.insetter}"
+    }
+    object Accompanist {
+        const val coil = "dev.chrisbanes.accompanist:accompanist-coil:${Versions.accompanist}"
     }
 }
