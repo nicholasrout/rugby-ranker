1308e073f12dec07eeac7487d23006fb86bd83f6
Nick Rout <nickrout@google.com>
2020-10-07 11:08:33 +0200

    Remove now unnecessary longPressGestureFilter from text

--- info/src/main/java/dev/ricknout/rugbyranker/info/ui/InfoFragment.kt
+++ info/src/main/java/dev/ricknout/rugbyranker/info/ui/InfoFragment.kt
@@ -39,7 +39,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.gesture.longPressGestureFilter
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
@@ -224,10 +223,7 @@ class InfoFragment : Fragment() {
             Text(
                 text = stringResource(R.string.version, version ?: ""),
                 style = MaterialTheme.typography.body1,
-                modifier = Modifier.fillMaxWidth().height(56.dp).padding(16.dp).longPressGestureFilter {
-                    // Do nothing here, prevents text selection on long press
-                    // See: https://issuetracker.google.com/issues/158459594
-                }
+                modifier = Modifier.fillMaxWidth().height(56.dp).padding(16.dp)
             )
         }
     }
