e4e800dd927f08dcaba92cb98be5266c78bd8817
Nick Rout <nickrout@google.com>
2021-04-08 13:15:57 +0200

    Update Compose dependencies

--- buildSrc/src/main/java/dev/ricknout/rugbyranker/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/dev/ricknout/rugbyranker/buildsrc/Dependencies.kt
@@ -4,11 +4,11 @@ object Versions {
     const val compileSdk = 30
     const val minSdk = 24
     const val targetSdk = 30
-    const val androidGradlePlugin = "7.0.0-alpha12"
+    const val androidGradlePlugin = "7.0.0-alpha14"
     const val ktlint = "0.40.0"
     const val coil = "1.1.1"
     const val insetter = "0.5.0"
-    const val accompanist = "0.7.0"
+    const val accompanist = "0.7.1"
     object Kotlin {
         const val kotlin = "1.4.31"
         const val coroutines = "1.4.3"
@@ -30,10 +30,10 @@ object Versions {
         const val work = "2.7.0-alpha02"
         const val hilt = "1.0.0-beta01"
         const val dataStore = "1.0.0-alpha08"
-        const val compose = "1.0.0-beta03"
+        const val compose = "1.0.0-beta04"
         object Lifecycle {
             const val lifecycle = "2.4.0-alpha01"
-            const val compose = "1.0.0-alpha03"
+            const val compose = "1.0.0-alpha04"
         }
         object Paging {
             const val paging = "3.0.0-beta03"
